b4c698e946851e07627613748d2e8116496205c7
Naix <91845220+NaixGames@users.noreply.github.com>
2025-03-15 16:35:58 +0000

    Finishing first dirty version of the data cleaner code (pun intended). Adding another stupid benchmark.

--- DataScripts/DataCleaner.py
+++ DataScripts/DataCleaner.py
@@ -1,6 +1,6 @@
 import numpy
 import pandas
-
+from datetime import datetime
 
 
 dtype_dict = {
@@ -45,50 +45,83 @@ dtype_dict = {
 	"Movies" : "str",
 }
 
+#Load dataframe and filter intended data
 dataframe = pandas.read_csv("../Data/93182_steam_games.csv", dtype = dtype_dict)
-
-dataframe = dataframe[["AppID", "Name", "Release date","Estimated owners", "Price", "Windows", "Mac", "Linux", "Positive", "Negative", "Genres"]]
+dataframe = dataframe[["AppID", "Name", "Release date","Estimated owners", "Price", "Windows", "Mac", "Linux", "Positive", "Negative", "Genres", "Categories"]]
 dataframe = dataframe[dataframe["Price"] > 0]
+dataframe = dataframe[dataframe["Estimated owners"].notnull()]
+dataframe["Release date"] = pandas.to_datetime(dataframe["Release date"], format='mixed')
+
+#Add the average owner column
+owners_column = dataframe["Estimated owners"].values
+average_owners = []
+for i in range(0,len(owners_column)):
+	owners = owners_column[i]
+	owners_range = owners.split("-")
+	average_owners.append((int(owners_range[0])+int(owners_range[1]))/2)
+
+dataframe["Average owners"] = average_owners
+
+#Add number of categories
+categories_column = dataframe["Categories"].values
+number_categories = []
+for i in range(0,len(owners_column)):
+	cat = str(categories_column[i])
+	cat_number = len(cat.split(","))
+	number_categories.append(cat_number)
+
+dataframe["Number categories"] = number_categories
+
+
+
+#Add the time since release to the data. Note that note the time the data was scrapped is 15th of September, 2024.
+date_column = dataframe["Release date"].values
+days_release = []
+scrap_day = numpy.datetime64("2024-08-15")
+for i in range(0,len(date_column)):
+	date = date_column[i]
+	dif_date = scrap_day-date+1
+	days_release.append(dif_date.astype('timedelta64[D]').astype(int))
+
+dataframe["Days since release"] = days_release
+#Avoiding picking up steam games that are up in the backend for playtesting
+dataframe = dataframe[dataframe["Days since release"] > 0]
+
+
+#Add the average owners per day
+average_owners = dataframe["Average owners"].values
+days_release = dataframe["Days since release"].values
+average_owners_per_day = []
+for i in range(0, len(days_release)):
+	average_owners_per_day.append(float(average_owners[i])/float(days_release[i]))
+
+dataframe["Average owners per day"] = average_owners_per_day
+
+
+
+#Estimated lifetime owners. For this we use https://newsletter.gamediscover.co/p/data-deep-dive-whats-the-long-tail
+#That estimates total sales by the first year you will sell in average 3 times your first week. I think
+#this is enough to determine a pareto distribution and from that I can actually have estimate for the whole life time
+#Indeed, assuming a pareto Distrubition F the approximation tell us F(365) = 3 F(7). Solving that gives the parameter of
+#the pareto is \alpha = 0.00537. So distribution is F(x) =  1 - x^(-0.00537). So then we can normalize time by
+#estimating the total lifetime owners by dividing by F(days since release!)
+
+#Add the estimated lifetime owners
+pareto_degree = 0.00537
+
+average_owners = dataframe["Average owners"].values
+days_release = dataframe["Days since release"].values
+estimated_total_owners = []
+
+for i in range(0, len(days_release)):
+	distribution_factor = 1-days_release[i]**(-pareto_degree)
+	distribution_factor = max(distribution_factor, pareto_degree)
+	estimation = average_owners[i]/distribution_factor
+	estimated_total_owners.append(estimation)
+
+dataframe["Estimated lifetime owners"] = estimated_total_owners
 
 dataframe.to_csv(
-    path_or_buf='../Data/data_test.csv',
-    sep=',',
-    na_rep='',
-    header=True,
-    index=True,
-    index_label=None,
-    mode='w',
-    storage_options={},
-    compression='infer',
-    chunksize=None,
-    date_format=None,
-    doublequote=True,
-    escapechar=None,
-    decimal='.',
-    errors='strict'
-)
-
-#Create a new data frame
-
-#iterate through every entry
-## if Price > 0 and reviews > 0 => add entry
-## for entry we add the AppID, Name, Release date, AVERAGE OWNERS!,
-## price, Platforms, Positive and negative reviews, categories, genres
-
-## I would also like to have Average owners / Time it was released (to normalize wrt to time)
-## for that note the time the data was scrapped is 15th of September, 2024.
-## For new inputs this is actually the quantity we want to estimate
-## Note that then we should not use the average owners for the estimation! We would be using the same data twice.
-
-##At some point I need to normalize values I think
-
-"""
-
-dataframe_dict = {"Test": [1,2,3,4]}
-
-new_df = pandas.DataFrame(data = dataframe_dict, columns = ["Test"])
-
-new_df.to_csv(
     path_or_buf='../Data/cleaned_data.csv',
     sep=',',
     na_rep='',
@@ -106,6 +139,12 @@ new_df.to_csv(
     errors='strict'
 )
 
-print("Data created")
-print(new_df)"
-"""
+## Todo: add a log object class to allow for detailed logging
+
+## split this using a strategy pattern
+
+##At some point I need to normalize values, but I will do so in a different script
+
+## make this part of a data pipeline, in which a call the cleaner and the normalizer
+
+
--- DumbBenchmarks/MultiColumn.py
+++ DumbBenchmarks/MultiColumn.py
@@ -2,21 +2,26 @@ import numpy
 import pandas
 import time
 
-#This is benchmark to test if caching an array before running through it is faster on pandas
-#For me it make sense, as it avoids a cache every call I think, but need to make sure :P
+#This is benchmark to test how slower it is to run through an array in different ways.
 
-N = 1000000
+N = 10000000
 
 
 
 dataframe = pandas.DataFrame({"A": range(1,N), "B": range(1,N), "C": range(1,N), "D": range(1,N), "E": range(1,N), "F": range(1,N), "G": range(1,N)})
 
 
+A_column = dataframe["A"]
+B_column = dataframe["B"]
+C_column = dataframe["C"]
+
 
 start_time = time.time()
 sum = 0
 for i in range(1,N-1):
-	sum += dataframe["A"][i]
+	sum += A_column[i]
+	sum += B_column[i]
+	sum += C_column[i]
 
 print("total sum " + str(sum))
 
@@ -31,9 +36,15 @@ print("The first method takes " + str(total_time))
 
 start_time = time.time()
 sum = 0
-A_column = dataframe["A"]
+
 for i in range(1,N-1):
 	sum += A_column[i]
+
+for i in range(1,N-1):
+	sum += B_column[i]
+
+for i in range(1,N-1):
+	sum += C_column[i]
 
 print("total sum " + str(sum))
 
@@ -42,4 +53,6 @@ total_time = end_time - start_time
 
 print("The second method takes " + str(total_time))
 
-#In my computer the second method takes HALF THE TIME :P. I thought it was going to be a bigger difference, but still!
+#In my computer both methods takes roughly the same time, with the second  one being SLIGHTLY faster. I think this
+#is due to caches misses, but I think pandas is doing some weird stuff under the hood, because I would expect the 
+#first method to be much slower (again, due to cache misses)
